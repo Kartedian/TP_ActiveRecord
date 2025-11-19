c6ab9c9e4cec367082e7d8e143146cff34f0a043
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-19 08:56:17 +0100

    [MODIFIED] ajoute les methodes pour test la classe DBConnection + renomme

---
+++ test/activeRecord/DBConnectionTest.java
@@ -0,0 +1,42 @@
+package activeRecord;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBConnectionTest {
+
+    @Test
+    void seulObjet(){
+        DBConnection.setConfig("./config/config.ini");
+        DBConnection instance = DBConnection.getInstance();
+
+        Connection connection = instance.getConnect();
+        assertNull(connection);
+
+        instance.setDbName("testpersonne");
+
+        connection = instance.getConnect();
+
+        assertNotNull(connection);
+
+        Connection connection2 = instance.getConnect();
+
+        assertEquals(connection, connection2);
+    }
+
+    @Test
+    void changerDB(){
+        DBConnection instance =  DBConnection.getInstance();
+        Connection connection = instance.getConnect();
+
+        assertNotNull(connection);
+
+        instance.setDbName("test");
+        Connection connection2 = instance.getConnect();
+
+        assertNotEquals(connection, connection2);
+    }
+}
