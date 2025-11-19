2b25846ef07d0eff6058d380f2d5b10505812e3d
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-19 09:17:42 +0100

    [MODIFIED] ajout des commentaires + renomme classe + correction

--- src/activeRecord/DBConnection.java
+++ src/activeRecord/DBConnection.java
@@ -6,46 +6,74 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import static java.util.Optional.empty;
+/**
+ * Classe DBConnection qui crée une connexion à notre système de gestion de base de données
+ */
+public class DBConnection {
 
-public class DBCConnection {
-
+    //nom d'utilisateur pour accéder
     private static String userName;
+    //mot de passe pour accéder
     private static String password;
+    //nom ou IP pour atteindre notre système de BDD
     private static String serverName;
+    //le port sur lequel il est hébergé
     private static String portNumber;
-    private static DBCConnection instance;
+    //instance
+    private static DBConnection instance;
 
+    //nom de la BDD à laquelle on veut accéder
     private String nameDB;
+    //url avec laquelle nous accéderons au système
     private String urlDB;
+    //objet que nous utiliserons pour atteindre la BDD et communiquer avec elle
     private Connection connect;
 
-    private DBCConnection() {
+    /**
+     * Constructeur de la classe
+     */
+    private DBConnection() {
         this.urlDB = "jdbc:mysql://"+ serverName+":"+portNumber+"/";
     }
 
-    public static DBCConnection getInstance(){
+    /**
+     * Méthode qui crée une instance si elle n'existe pas (Singleton).
+     *
+     * @return
+     */
+    public static DBConnection getInstance(){
         if(instance == null){
-            instance = new DBCConnection();
+            instance = new DBConnection();
         }
         return instance;
     }
 
+    /**
+     * Méthode qui utilise un fichier, passé en paramètre, pour extraire les données d'accès au système de gestion
+     * de BDD.
+     *
+     * @param file
+     */
     public static void setConfig(String file){
         Properties prop = new Properties();
         try(FileInputStream fis = new FileInputStream(file)){
             prop.load(fis);
 
-            DBCConnection.userName = prop.getProperty("user");
-            DBCConnection.password = prop.getProperty("password");
-            DBCConnection.serverName = prop.getProperty("serverName");
-            DBCConnection.portNumber = prop.getProperty("portNumber");
+            DBConnection.userName = prop.getProperty("user");
+            DBConnection.password = prop.getProperty("password");
+            DBConnection.serverName = prop.getProperty("serverName");
+            DBConnection.portNumber = prop.getProperty("portNumber");
 
         }catch(Exception e){
             e.printStackTrace();
         }
     }
 
+    /**
+     * Méthode qui crée la connexion avec notre BDD si elle n'existe pas, sinon renvoie celle déjà existante.
+     *
+     * @return
+     */
     public Connection getConnect(){
         if(connect == null){
             try{
@@ -67,7 +95,12 @@ public class DBCConnection {
         return this.connect;
     }
 
-    public void setDbName(String dbName){
+    /**
+     * Méthode set pour le nom de la BDD à laquelle on voudrait accéder.
+     *
+     * @param dbName
+     */
+    public void setBDName(String dbName){
         nameDB=dbName;
         connect=null;
     }
