67dc629d8c66a9f82711182415a5da5e2c9663a6
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-21 12:25:17 +0100

    [ADDED] ajout les attributs et les constructeurs

--- src/activeRecord/Film.java
+++ src/activeRecord/Film.java
@@ -1,4 +1,21 @@
 package activeRecord;
 
 public class Film {
+
+    private String titre;
+    private int id;
+    private int id_real;
+
+    public Film(String titre, Personne personne){
+        this.titre = titre;
+        this.id_real = personne.getId();
+        this.id = -1;
+    }
+
+    private Film(String titre, int id, int id_real){
+        this.titre = titre;
+        this.id = id;
+        this.id_real = id_real;
+    }
+
 }
