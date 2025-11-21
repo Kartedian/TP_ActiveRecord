8dd540185caf6e9562b50973718523a704b8d465
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-21 13:39:05 +0100

    [ADDED] ajout les methodes findById, getRealisateur, createTable et deleteTable

--- src/activeRecord/Film.java
+++ src/activeRecord/Film.java
@@ -18,4 +18,59 @@ public class Film {
         this.id_real = id_real;
     }
 
+    public static Film findById(int id){
+        try{
+            Connection connection = dbConnection.getConnect();
+
+            String query = "SELECT * FROM Film WHERE id = ?;";
+
+            PreparedStatement sql = connection.prepareStatement(query);
+            sql.setInt(1, id);
+            sql.execute();
+
+            ResultSet rs = sql.getResultSet();
+
+            if(rs.next()){
+                return new Film(rs.getString("titre"), rs.getInt("id"), rs.getInt("id_rea"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public Personne getRealisateur(){
+        try {
+            return Personne.findById(this.id_real);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void createTable(){
+        Connection connection = dbConnection.getConnect();
+        try{
+            String createString = "CREATE TABLE Film (" +
+                    " ID INTEGER AUTO_INCREMENT, "+
+                    " TITRE varchar(40) NOT NULL, " +
+                    " ID_REA int(11) DEFAULT NULL" +
+                    " PRIMARY KEY (ID), " +
+                    " FOREIGN KEY (ID_REA) REFERENCES Personne(ID))";
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteTable(){
+        Connection connection = dbConnection.getConnect();
+        try{
+            String createString = "DROP TABLE Film";
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
